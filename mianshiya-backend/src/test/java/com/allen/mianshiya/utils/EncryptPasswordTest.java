799efb99fffae0e19aee6f4528dcd8a8f98f7c7d
KiveAllen <1348438509@qq.com>
2024-10-22 15:53:11 +0800

    feat(backend: Test): 添加密码加密功能并更新相关测试
    - 在 CommonConstant 类中添加 SALT常量用于密码加密
    - 新增 EncryptPasswordTest 类进行密码加密测试
    - 更新 init_data.sql 中的用户密码，使用新的加密方式
    - 修改 UserServiceImpl 中的密码加密逻辑，使用 CommonConstant 中的 SALT

---
+++ mianshiya-backend/src/test/java/com/allen/mianshiya/utils/EncryptPasswordTest.java
@@ -0,0 +1,24 @@
+package com.allen.mianshiya.utils;
+
+import com.allen.mianshiya.constant.CommonConstant;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.DigestUtils;
+
+/**
+ * EasyExcel 测试
+ *
+ */
+@SpringBootTest
+public class EncryptPasswordTest {
+
+    @Test
+    public void getEncryptPassword() {
+        // 加密
+        String password = "12345678";
+        String encryptPassword = DigestUtils.md5DigestAsHex((CommonConstant.SALT + password).getBytes());
+        System.out.println(encryptPassword);
+    }
+
+
+}
